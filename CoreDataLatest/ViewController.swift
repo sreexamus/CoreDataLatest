f1b1e3b836cfbe5df2eaaa8d97c6af5f773872fc
sreekanth <sreekanthreddy.iragamreddy@schwab.com>
2019-06-08 12:37:12 -0500

    Initial Commit

---
+++ CoreDataLatest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataLatest
+//
+//  Created by Iragam Reddy, Sreekanth Reddy on 6/8/19.
+//  Copyright © 2019 Iragam Reddy, Sreekanth Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
